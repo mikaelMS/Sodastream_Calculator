303f6ce3cea595a16e6d7f7100930b5c476c4adf
mikaelMS <schmidt.m.work@googlemail.com>
2019-05-08 22:27:33 +0200

    Revert "fully functional calculator, adjusted to the site style"
    This reverts commit a04b1c410d2a9b255770959124f1d942fbcebc90.

--- js/refactoredCalculator.js
+++ js/refactoredCalculator.js
@@ -1,55 +1,14 @@
-var startWindowSize = 0;
-$(window).on('load', function() {
-  startWindowSize = $(this).width();
-  console.log(startWindowSize);
-});
-
-var whatLiterToChoose = "";
-// Window resize function
-$(window).on('resize', function() {
-  var win = $(this); //this = window
-  console.log("hier");
-  if (win.width() > 762) {
-    whatLiterToChoose = "scale";
-  }
-  if (win.width() <= 762) {
-    whatLiterToChoose = "slider";
-  }
-
-  if (startWindowSize - win.width() < 0 && win.width() > 762) {
-    inputs[2] = false
-    enableButton();
-  } else {
-    inputs[2] = true
-    enableButton();
-  }
-
-  if (startWindowSize - win.width() > 0 && win.width() <= 762) {
-    inputs[2] = false
-    enableButton();
-  } else {
-    inputs[2] = true
-    enableButton();
-  }
-});
-
 // Range Slider
 var max = 40,
-  min = 0.5,
-  step = 0.5,
-  value = 0;
-output = $('#range-slider__value').text(value.toFixed(1));
-coolNumber = value;
+    min = 0.5,
+    step = 0.5,
+    value = 0;
+    output = $('#range-slider__value').text(value.toFixed(1));
+    coolNumber = value;
 
 let Calculator = {
   // Price per liter water in euros
   WATER_PRICE: 0.002,
-
-  // Price dicounter water per liter in euros
-  WATER_PRICE_DISCOUNTER: 0.13,
-
-  // Price marken water per liter in euros
-  WATER_PRICE_MARKEN: 0.30,
 
   // Equipment cost per month in euros
   EQUIPMENT_COST: 0.25,
@@ -74,14 +33,8 @@ let Calculator = {
   CYCLINDER_PRICE: 9,
 
   // Results
-  LiterResultSodastream: 0,
-  MonthResultSodastream: 0,
-
-  LiterResultDiscounter: 0,
-  MonthResultDiscounter: 0,
-
-  LiterResultMarken: 0,
-  MonthResultMarken: 0,
+  LiterResult: 0,
+  MonthResult: 0,
 
   startProcess: function() {
     let gasSelect = document.getElementById("choice_gas_power");
@@ -90,13 +43,13 @@ let Calculator = {
     let sodastreamNumber = document.getElementById("choice_sodastream");
     let sodaChoice = sodastreamNumber.options[sodastreamNumber.selectedIndex].value;
 
-    let literChoice;
+    var literChoice;
 
-    console.log(whatLiterToChoose);
-    if (whatLiterToChoose === "scale" || $(window).width() > 762) {
+    if($(window).width() > 762){
       literChoice = document.getElementById("choice_liters").value;
+      console.log(literChoice);
     } else {
-      literChoice = coolNumber;
+        literChoice = coolNumber;
     }
 
     Calculator.calculateLiterPrice(Calculator.getGasCoverage(gasChoice), Calculator.getSStreamerPrice(sodaChoice), literChoice);
@@ -121,13 +74,7 @@ let Calculator = {
     let num = Number(result * 100);
     let roundedString = num.toFixed(2);
     let rounded_result = String(roundedString);
-    Calculator.LiterResultSodastream = rounded_result.replace(".", ",");
-
-    let discounterResult = String((literAmount * Calculator.WATER_PRICE_DISCOUNTER).toFixed(2));
-    Calculator.LiterResultDiscounter = discounterResult.replace(".", ",");
-
-    let markenResult = String((literAmount * Calculator.WATER_PRICE_MARKEN).toFixed(2));
-    Calculator.LiterResultMarken = markenResult.replace(".", ",");
+    Calculator.LiterResult = rounded_result.replace(".", ",");
   },
 
   calculateMonthPrice: function(gasCoverage, sodaPrice, literAmount) {
@@ -148,13 +95,7 @@ let Calculator = {
     let num = Number(result);
     let roundedString = num.toFixed(2);
     let rounded_result = String(roundedString);
-    Calculator.MonthResultSodastream = rounded_result.replace(".", ",");
-
-    let discounterResult = String((literAmount * Calculator.WATER_PRICE_DISCOUNTER * 30).toFixed(2));
-    Calculator.MonthResultDiscounter = discounterResult.replace(".", ",");
-
-    let markenResult = String((literAmount * Calculator.WATER_PRICE_MARKEN * 30).toFixed(2));
-    Calculator.MonthResultMarken = markenResult.replace(".", ",");
+    Calculator.MonthResult = rounded_result.replace(".", ",");
   },
 
   getSStreamerPrice: function(sodaChoice) {
@@ -217,19 +158,13 @@ let Calculator = {
 };
 
 // JQuery Event handling
-var inputs = new Array(false, false, false);
+let inputs = new Array(false, false, false);
 
 $("#btn_result").click(function(e) {
   e.preventDefault();
   Calculator.startProcess();
-  document.getElementById('result_liter_sodastream').innerHTML = Calculator.LiterResultSodastream + " " + "Cent";
-  document.getElementById('result_month_sodastream').innerHTML = Calculator.MonthResultSodastream + "€";
-
-  document.getElementById('result_liter_discounter').innerHTML = Calculator.LiterResultDiscounter + " " + "Cent";
-  document.getElementById('result_month_discounter').innerHTML = Calculator.MonthResultDiscounter + "€";
-
-  document.getElementById('result_liter_marken').innerHTML = Calculator.LiterResultMarken + " " + "Cent";
-  document.getElementById('result_month_marken').innerHTML = Calculator.MonthResultMarken + "€";
+  document.getElementById('result_liter').innerHTML = Calculator.LiterResult + " " + "Cent";
+  document.getElementById('result_month').innerHTML = Calculator.MonthResult +"€";
   $("#results_table tr").fadeIn("slow");
 });
 
@@ -252,36 +187,25 @@ $("#choice_sodastream").on('change', function() {
 });
 
 $("#choice_liters").on('change', function() {
-  if ((startWindowSize - 762) > 0) {
-    inputs[2] = true;
-    enableButton();
-  }
+  inputs[2] = true;
+  enableButton();
 });
 
 $("#formControlRange")
-  .attr({
-    'max': max,
-    'min': min,
-    'step': step,
-    'value': String(value)
-  })
-  .on('input change', function() {
+    .attr({'max': max, 'min':min, 'step': step,'value': String(value)})
+    .on('input change', function() {
 
-    coolNumber = parseFloat(this.value);
-    output.text(coolNumber.toFixed(1));
+        coolNumber = parseFloat(this.value);
+        output.text(coolNumber.toFixed(1));
 
-    if (document.getElementById("formControlRange").value > 0) {
-      if ((startWindowSize - 762) <= 0) {
-        inputs[2] = true;
-        enableButton();
-      }
-    }
-  });
+        if(document.getElementById("formControlRange").value > 0) {
+          inputs[2] = true;
+          enableButton();
+        }
+    });
 
 function enableButton() {
   if (inputs[0] && inputs[1] && inputs[2]) {
     $("#btn_result").prop('disabled', false);
-  } else {
-    $("#btn_result").prop('disabled', true);
   }
 }
